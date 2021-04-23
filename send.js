b10a5d885523f5c5d9d65fac3ddbf88679095537
Aliaksandr <archon.san@Gmail.com>
2021-04-23 19:23:38 +0300

    fix not work

--- send.js
+++ send.js
@@ -4,7 +4,7 @@ const form = document.getElementById('form')
 const url = 'http://194.120.116.33:8082/message/'
 
 form.addEventListener('submit', e => {
-
+    alert("Пока что не работает")
     e.preventDefault()
     fetch(url + email.value)
         .then(response => response.json())
