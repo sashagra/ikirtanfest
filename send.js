b0524ca0b5e0b754c786188703278883db92fda5
Aliaksandr <archon.san@Gmail.com>
2021-04-24 11:36:48 +0300

    fix: message

--- send.js
+++ send.js
@@ -6,7 +6,8 @@ const url = 'https://minsk-hk-news.herokuapp.com/message/'
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(url + email.value)
-        .then(response => response.json())
-        .then(r => console.log(r))
+    .then(response => response.json())
+    .then(r => console.log(r))
+    alert("Спасибо. Мы записали ваш адрес. Обещаем, никакого спама")
     
 })
